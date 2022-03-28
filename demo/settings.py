f4699da48b4a802f0f4da9f5639550dceb72a4b1
bardolphshampine <90364136+butterfly1924@users.noreply.github.com>
2022-03-28 15:06:58 +0000

    gunicorn

--- demo/settings.py
+++ demo/settings.py
@@ -23,7 +23,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-n##tq^x=@y=fgm6lk+38xfa8e8kgg%kh9&ov!p66j4(!-y3v1*'
 
-
 SECURE_SSL_REDIRECT = False
 
 SECURE_HSTS_INCLUDE_SUBDOMAINS = False
